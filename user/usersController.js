c97185c5c3cd39bb7e8433448923e22b38080272
gabrielrpaladini <gabrielpaladini@outlook.com.br>
2021-04-11 17:25:58 -0300

    adding logout

--- user/usersController.js
+++ user/usersController.js
@@ -72,4 +72,9 @@ router.post("/authenticate", (req, res) => {
     })
 });
 
+router.get("/logout", (req, res) =>{
+    req.session.user = undefined;
+    res.redirect("/");
+})
+
 module.exports = router;
