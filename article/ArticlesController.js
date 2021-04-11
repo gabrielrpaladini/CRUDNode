48e95b1213601a9b32897468299463e644166d98
gabrielrpaladini <gabrielpaladini@outlook.com.br>
2021-04-11 16:20:16 -0300

    adding pagination system

--- article/ArticlesController.js
+++ article/ArticlesController.js
@@ -90,4 +90,45 @@ router.post("/articles/update", (req, res) => {
 
 });
 
+router.get("/articles/page/:num", (req, res) => {
+
+    let page = req.params.num;
+    let offset = 0;
+    let itensPerPage = 4;
+
+    // Logica de paginação
+    if(isNaN(page) || page == 1) {
+        offset = 0;
+    } else{
+        offset = (parseInt(page) - 1) * itensPerPage;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset,
+        order: [
+            ['id', 'DESC']
+        ]
+    }
+    ).then(articles => {
+
+        let next;
+        if(offset + 4 >= articles.count) {
+            next = false;
+        } else {
+            next = true;
+        }
+
+        let result = {
+            page: parseInt(page),
+            next: next,
+            articles: articles
+        }
+
+        Category.findAll().then(categories => {
+            res.render("/admin/articles/page", {result: result, categories: categories});
+        });
+    });
+});
+
 module.exports = router;
